c394cec6d84aaf93bbbf686f7a4c09a939be2ff5
worksofallwin <princeallwin199@gmail.com>
2023-07-24 17:09:19 +0530

    Add Herlper Config

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./Helperconfig.s.sol";
 
 contract DeployFundMe is Script {
     FundMe fundMe;
+    HelperConfig helperConfig = new HelperConfig();
+    address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
 
     function run() external returns (FundMe) {
         vm.startBroadcast();
-        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
 
         return fundMe;
